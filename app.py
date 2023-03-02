069f950eec76882a0439373d6dbefbc330760a2a
Matthh69 <m.duportbutique@ipilyon.net>
2023-03-02 16:19:06 +0100

    Update for templates folder

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template ('index.html') #"<p>Hello, World!</p>"
