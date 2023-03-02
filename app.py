706df08ec0b33f138bfd8eeb3f29cd9dbf71109f
libap <limandribaptiste@gmail.com>
2023-03-02 16:04:13 +0100

    Merge branch 'main' of https://github.com/Matthh69/calendarpy

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-
+# add commit pull push
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-    return render_template ('index.html') #"<p>Hello, World!</p>"
+    return render_template('index.html')  # "<p>Hello, World!</p>"
