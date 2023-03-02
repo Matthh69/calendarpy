8552f0733f156631e2414aeac96778598726188c
Matthh69 <m.duportbutique@ipilyon.net>
2023-03-02 15:56:18 +0100

    Updated app.py with render_template

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-    # commentai
+    return render_template ('index.html') #"<p>Hello, World!</p>"
