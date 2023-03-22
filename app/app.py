00e732ca85a209446c921b8f520fbae11a20d6f2
libap <limandribaptiste@gmail.com>
2023-03-22 17:59:24 +0100

    database.db commande  discord puis print in html

--- app/app.py
+++ app/app.py
@@ -1,16 +1,27 @@
+import sqlite3
+from flask import g
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#Routing index
+# Routing index
+
+
 @app.route("/")
 def hello_world():
     return render_template('index.html')
 
-#Routing calendar
+# Routing calendar
+
+
 @app.route('/calendar')
 def hello():
-    return render_template('calendar.html')
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM event")
+    events = c.fetchall()
+    return render_template('calendar.html', events=events)
 
 
 @app.errorhandler(404)
