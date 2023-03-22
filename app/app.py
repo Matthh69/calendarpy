02a2e94540081cc503b6256d0405de553a4666ef
libap <limandribaptiste@gmail.com>
2023-03-22 16:22:14 +0100

    /hello

--- app/app.py
+++ app/app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
