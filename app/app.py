ca29617f476b7475299018067f221c8be5caaf54
libap <limandribaptiste@gmail.com>
2023-03-23 17:28:35 +0100

    ajout condition dans app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,21 @@
 import sqlite3
+import os
 from flask import g
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# fonction pour accéder à la base de données
+
+
+def get_events():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM event")
+    events = cursor.fetchall()
+    conn.close()
+    return events
+
 
 # Routing index
 
@@ -16,12 +29,12 @@ def hello_world():
 
 @app.route('/calendar')
 def hello():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-
-    c.execute("SELECT * FROM event")
-    events = c.fetchall()
-    return render_template('calendar.html', events=events)
+    if not os.path.isfile('database.db'):
+        return render_template('noDatabase.html')
+    else:
+        # si la base de données existe déjà, récupérer les données de la table events
+        events = get_events()
+        return render_template('calendar.html', events=events)
 
 
 @app.errorhandler(404)
