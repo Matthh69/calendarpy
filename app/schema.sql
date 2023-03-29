ad74a2896605550d3c406778e6086c9bbd0e14af
Matthh69 <m.duportbutique@ipilyon.net>
2023-03-29 17:04:37 +0200

    init_db command implemented and functional

--- app/schema.sql
+++ app/schema.sql
@@ -7,7 +7,7 @@
 --Table to store calendar
 DROP TABLE IF EXISTS calendar;
 CREATE TABLE calendar (
-    cal_id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT
     --link TEXT // Will use id instead
 );
 
@@ -17,7 +17,7 @@ CREATE TABLE event (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     event_date DATETIME, --Format ('YYYY-MM-DD hh:mm:ss')
     txt_content TEXT,
-    id_cal INTEGER REFERENCES calendar (cal_id));
+    id_cal INTEGER REFERENCES calendar (id));
 
 --Creation of dummy content
 
@@ -26,11 +26,11 @@ INSERT INTO calendar VALUES (1);
 INSERT INTO calendar VALUES (2);
 
 --Messages
-INSERT INTO event VALUES (1, '2022-10-10 11:24:55', 'Premier message', 1);
-INSERT INTO event VALUES (2, '2022-10-10 15:54:06', 'Maths en L697', 1);
-INSERT INTO event VALUES (3, '2022-10-14 08:11:25', 'Histoire géo en L265', 1);
+INSERT INTO event (event_date, txt_content, id_cal) VALUES ('2022-10-10 11:24:55', 'Premier message', 1);
+INSERT INTO event (event_date, txt_content, id_cal) VALUES ('2022-10-10 15:54:06', 'Maths en L697', 1);
+INSERT INTO event (event_date, txt_content, id_cal) VALUES ('2022-10-14 08:11:25', 'Histoire géo en L265', 1);
 
 
-INSERT INTO event VALUES (1, '2023-10-10 11:24:55', 'Premier message', 2);
-INSERT INTO event VALUES (2, '2023-10-10 15:54:06', 'Maths en L697', 2);
-INSERT INTO event VALUES (3, '2023-10-14 08:11:25', 'Histoire géo en L265', 2);
+INSERT INTO event (event_date, txt_content, id_cal) VALUES ('2023-10-10 11:24:55', 'Premier message', 2);
+INSERT INTO event (event_date, txt_content, id_cal) VALUES ('2023-10-10 15:54:06', 'Maths en L697', 2);
+INSERT INTO event (event_date, txt_content, id_cal) VALUES ('2023-10-14 08:11:25', 'Histoire géo en L265', 2);
