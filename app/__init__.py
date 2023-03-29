b1d8adacdd58be20be49d5c82d9ac6693b31f60a
Matthh69 <m.duportbutique@ipilyon.net>
2023-03-29 17:43:37 +0200

    aded __init__

---
+++ app/__init__.py
@@ -0,0 +1,54 @@
+import os
+import sqlite3
+from flask import Flask, render_template
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'database.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Routing index
+    @app.route("/")
+    def hello_world():
+        return render_template('index.html')
+
+    # Routing calendar
+    @app.route('/calendar')
+    def hello():
+        conn = sqlite3.connect('instance/database.sqlite')
+        cursor = conn.cursor()
+
+        # Récupération de tous les événements
+        cursor.execute('SELECT * FROM event')
+        events = cursor.fetchall()
+        return render_template('calendar.html', events=events)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('page_not_found.html'), 404
+
+    """
+        DATABASE
+    """
+
+    from . import db
+    db.init_app(app)
+
+    return app
